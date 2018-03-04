8d7e4743c5a53c8aa7b1e42d780903d6111bd8c4
Scott <rince69@gmail.com>
2018-03-04 19:01:35 +1300

    graph added

---
+++ Get-SolarData.ps1
@@ -0,0 +1,18 @@
+﻿$CSVs = Get-ChildItem . | ?{$_.Extension -eq ".csv"}
+
+write-host "tilt,bearing,global energy,diffuse energy,tilted energy"
+foreach ($csv in $CSVs) {
+$filename = $csv.name
+$file = Import-Csv $filename
+
+$tenergy = 0
+$genergy = 0
+$denergy = 0
+foreach ($item in $file) {
+$tenergy += $item.'Tilted Irr.'
+$genergy += $item.'Global Irr.'
+$denergy += $item.'Diffuse Irr.'
+}
+write-host "$($filename.Substring(4,2)),$($filename.Substring(12,2)),$genergy,$denergy,$tenergy"
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SolarView
+
+Crunch the raw data from NIWA's solarView application
+(Of no interest to anyone else!)
